cd36e9c3d3447df4d784c5b7b20028935129e942
Abhinav Jha <abhinav.jha58@gmail.com>
2020-06-19 09:00:12 +0530

    initial Commit

---
+++ precache-manifest.9788222c5c0bb224c0fd226872a8f886.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a5226e165860df43a1e3b69e6eff42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b074b66abe9445b2792",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2a87ff5cda77916758d3",
+    "url": "/static/css/main.88c93369.chunk.css"
+  },
+  {
+    "revision": "9b074b66abe9445b2792",
+    "url": "/static/js/2.49d07d85.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.49d07d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a87ff5cda77916758d3",
+    "url": "/static/js/main.c9e10b14.chunk.js"
+  },
+  {
+    "revision": "0860c5403434ef7349aa",
+    "url": "/static/js/runtime-main.cf98fd41.js"
+  }
+]);
